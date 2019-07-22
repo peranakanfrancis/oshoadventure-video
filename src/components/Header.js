10b1daa799dea3166b95d20dcc2c1ebb80634fac
Peranakan Francis <francis.tch@gmail.com>
2019-07-22 17:56:55 +0545

    first to publish

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,8 +9,7 @@ const Header = (props) => (
         <div className="content">
             <div className="inner">
                 <h1>Osho World Adventure</h1>
-                <h3>Nepal's first one-stop solution for travel agents and corporate, gain access to a wide breadth of quality travel products, impressive rates, last-minute availability, and a world-class booking experience including hotels, tours, activities, transfers and car rentals.</h3>
-                <p>With our travel professionals, account managers, customer service, diversified products with dynamic pricing, and variety of ways to connect to our platform, you’ll see how Osho World Adventure is quickly becoming a household name in the travel industry. Request your login today to see why over 20,000 travel trade partners continue to travel with us.</p>
+                <p>Nepal's first one-stop solution for travel agents and corporate, gain access to a wide breadth of quality travel products, impressive rates, last-minute availability, and a world-class booking experience including hotels, tours, activities, transfers and car rentals.</p>
             </div>
         </div>
         <nav>
